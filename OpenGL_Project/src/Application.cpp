cba84651a82bedd702f00e7e13f47d616df3697b
Sidney-Land <136678741+Sidney-Land@users.noreply.github.com>
2025-11-19 16:18:44 -0500

    [Outside Tutorial] Added Comments to Application.cpp
    + Added signature and generic program description.
    
    + Changed existing comments to fit my single line comment style.
    
    + Added small clarifications I thought comments were lacking.

--- OpenGL_Project/src/Application.cpp
+++ OpenGL_Project/src/Application.cpp
@@ -1,3 +1,9 @@
+/*
+Author: Sidney Land
+
+This program handles the main thread of this OpenGL project.
+*/
+
 // Must include GLEW (GL) before GLFW
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -7,6 +13,7 @@
 
 int main(void)
 {
+    // Declare a GLFW window
     GLFWwindow* window;
 
     // Initialize the GLFW window/context manager
@@ -16,7 +23,7 @@ int main(void)
         return -1;
     }
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Create a windowed mode window and its OpenGL context
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -24,7 +31,7 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current
     glfwMakeContextCurrent(window);
 
     // Initialize GLEW extension manager (must be done after a context is made current)
@@ -34,15 +41,16 @@ int main(void)
         return -1;
     }
 
+    // Print the current OpenGL version being used
     printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
     
     // Only update display buffer (by swapping the double buffers) after 1 frame has been fully generated (enables vsync)
     glfwSwapInterval(1);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Clears color buffer for next frame
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draws a white triangle with Legacy OpenGL
@@ -52,13 +60,16 @@ int main(void)
         glVertex2f(0.5f, -0.5f);
         glEnd();
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers, refreshing display
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events, like closing the window
         glfwPollEvents();
     }
 
+    // Terminate GLFW
     glfwTerminate();
+
+    // Terminate Program
     return 0;
 }
